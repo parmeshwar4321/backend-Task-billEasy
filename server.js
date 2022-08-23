fdd73947f5b1f620904e8a6b1aed5b6c13ef824d
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-08-23 10:36:44 +0530

    added error handler

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const PORT = 8080;
 
 const employee = require("./routes/employee");
 const department = require("./routes/department");
+const middleware = require("./utils/middleware");
+
 var corsOptions = {
   Origin: "['http://localhost:3000]",
   optionsSuccessStatus: 200,
 }; // For legacy browser support
 app.use(cors(corsOptions));
 app.use(express.json());
-
+app.get("/", (req, res) =>
+  res.send({ msg: "you are connected to application" })
+);
 app.use("/api", employee);
 app.use("/api", department);
+
+//errroHandelers
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
